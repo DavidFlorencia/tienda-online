787410fc840f993e7a1b8228bd2d96e9e163b725
it-dflorenciac <dflorencia.developer@gmail.com>
2025-07-19 13:08:14 -0600

    create main components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListadoProductos } from "./listado-productos/listado-productos";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListadoProductos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected title = 'tienda-online';
+  titulo = 'Tienda Online';
 }

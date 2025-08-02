fcd9cc4b6fc3af95bcba60fec2ada5768222b542
it-dflorenciac <dflorencia.developer@gmail.com>
2025-08-02 09:41:40 -0600

    list of products using for

--- src/app/listado-productos/listado-productos.ts
+++ src/app/listado-productos/listado-productos.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { Producto } from "../producto/producto";
+import { Producto } from "../producto/producto.model";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listado-productos',
-  imports: [Producto],
+  imports: [FormsModule],
   templateUrl: './listado-productos.html',
   styleUrl: './listado-productos.css'
 })
 export class ListadoProductos {
+  productos: Producto[] = [
+    new Producto("Pantalon", 130.0),
+    new Producto("Camisa", 80.0),
+    new Producto("Playera", 50.0)
+  ];
 
+  descripcionInput: string = "";
+  precioInput: number | null = null;
+
+  agregarProducto() {
+    if (this.descripcionInput.trim() === '' || this.precioInput == null || this.precioInput <= 0) {
+      console.log("Por favor, ingrese una descripción válida y un precio mayor a 0.");
+      return;
+    }
+
+    this.productos.push(new Producto(this.descripcionInput, this.precioInput));
+    this.descripcionInput = "";
+    this.precioInput = null;
+  }
 }
